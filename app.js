984acd3eeb0b3d095a4fcdb344554cb427bad182
Julkarnain Shahid <jshahid1997@gmail.com>
2020-04-23 20:25:46 +0530

    Configuring HTTPS

--- app.js
+++ app.js
@@ -32,7 +32,15 @@ var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
 
 var app = express();
-
+app.all("*", (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  res.redirect(
+    307,
+    "https://" + req.hostname + ":" + app.get("secPort") + req.url
+  );
+});
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
