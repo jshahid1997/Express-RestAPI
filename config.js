76d332eaac4d23f4c8fd6e9d6b081883256e8f28
Julkarnain Shahid <jshahid1997@gmail.com>
2020-04-25 18:49:04 +0530

    OAuth using Passport-Fb_token

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/conFusion",
+  facebook: {
+    clientId: "169209654331531",
+    clientSecret: "f81dabe767f45920dca9b964f59c2e00",
+  },
 };
